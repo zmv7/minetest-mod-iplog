5c7da82d6d5db30e74d8383b99ca532cf27c168a
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-15 01:52:12 +0500

    corrections

--- init.lua
+++ init.lua
@@ -9,17 +9,17 @@ end)
 core.register_chatcommand("iplog",{
   description = "Get your IP history or another player's",
   privs = {interact=true},
-  params = "[playername] [--purge] [--purge-all]",
+  params = "[playername] [/purge] [/purge-all]",
   func = function(name,param)
     local nick = name
     local admin = core.check_player_privs(name,{server=true})
     if param and param ~= "" and admin then
         nick = param:match("^%S+")
     end
-    if param:match("%-%-purge$") and admin then
+    if param:match("/purge$") and admin then
         s:set_string(nick,"")
         return true, "IPLog of "..nick.." purged"
-    elseif param:match("%-%-purge%-all") and admin then
+    elseif param:match("/purge%-all") and admin then
         local stable = s:to_table().fields
         local count = 0
         for nick,hist in pairs(stable) do
