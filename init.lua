b1338d40f6be4f25a99dba3b03bd87004068a1bc
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2024-06-22 01:38:04 +0500

    Add latest_count parameter

--- init.lua
+++ init.lua
@@ -1,39 +1,42 @@
-local s = core.get_mod_storage()
+local s = minetest.get_mod_storage()
 
-core.register_on_authplayer(function(name, ip, is_success)
+minetest.register_on_authplayer(function(name, ip, is_success)
     local date = os.date()
     local hist = s:get_string(name)
     s:set_string(name,hist..date.." - "..ip..(is_success and "\n" or " /!\\ FAIL\n"))
 end)
 
-core.register_chatcommand("iplog",{
+minetest.register_chatcommand("iplog",{
   description = "Get your IP history or another player's",
   privs = {interact=true},
-  params = "[playername] [/purge] [/purge-all]",
+  params = "[[playername] [latest_count] | [/purge]] | [/purge-all]",
   func = function(name,param)
     local nick = name
-    local admin = core.check_player_privs(name,{server=true})
-    if param and param ~= "" and admin then
-        nick = param:match("^%S+")
+    local admin = minetest.check_player_privs(name,{server=true})
+    local params = param:split(" ")
+    if admin and params[1] then
+        nick = params[1]
     end
-    if param:match("/purge$") and admin then
+    if admin and params[2] == "/purge" then
         s:set_string(nick,"")
         return true, "IPLog of "..nick.." purged"
-    elseif param:match("/purge%-all") and admin then
-        local stable = s:to_table().fields
+    end
+    if admin and params[1] == "/purge-all" then
         local count = 0
-        for nick,hist in pairs(stable) do
+        for nick,hist in pairs(s:to_table().fields) do
             s:set_string(nick,"")
             count = count + 1
         end
         return true, "IPLog totally purged, there were "..count.." entries"
     end
     local lines = s:get_string(nick):split("\n")
-    if not lines then
-        return false, "Eror getting IPLog of "..nick
+    if not lines or #lines == 0 then
+        return false, "There is no IPLog of "..nick
     end
-    for _,line in ipairs(lines) do
-        core.chat_send_player(name,line)
+    for i,line in ipairs(lines) do
+        if not tonumber(params[2]) or i > #lines - tonumber(params[2]) then
+            minetest.chat_send_player(name,line)
+        end
     end
     return true, "IPLog of "..nick.." listed"
 end})
